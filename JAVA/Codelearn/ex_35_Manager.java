644a3101dd794a3ecaf9058f8010a75368a7ebf5
tungNguyen0203 <nguyenxuantung0203t@gmail.com>
2021-10-13 00:21:31 +0700

    ex_35

---
+++ JAVA/Codelearn/ex_35_Manager.java
@@ -0,0 +1,29 @@
+package entity;
+
+import com.sun.beans.introspect.PropertyInfo.Name;
+
+public class ex_35_Manager extends ex_35_Employee{
+	private int bonus;
+	
+	public ex_35_Manager(String name, int salary, int bonus) {
+		super(name, salary);
+		this.bonus = bonus;
+	}
+
+	public int getBonus() {
+		return bonus;
+	}
+	// gaf
+
+	public void setBonus(int bonus) {
+		this.bonus = bonus;
+	}
+	public int getSalary() {
+		return super.getSalary() + bonus;
+	}
+	@Override
+	public void display() {
+		System.out.println("Name: " + getName());
+		System.out.println("Salary: " + getSalary());
+	}
+}
